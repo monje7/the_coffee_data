15e56d041becc4a48f45264106e3708a85fa4d12
danielcordoba12 <dcordobaruiz@gmail.com>
2023-11-02 20:50:24 -0500

    documentacion dcr

--- back-end/src/controllers/muestras.controller.js
+++ back-end/src/controllers/muestras.controller.js
@@ -6,11 +6,9 @@ export const guardarMuestra = async (req, res) => {
     try {
         let data = req.body;
 
-        let sql =  `INSERT INTO muestras(fecha_creacion, nombre, cantidad,lotes_id ) VALUES (?,?,?,?)`
+        let sql =  `INSERT INTO muestras( fecha_creacion, nombre, cantidad, lotes_id, muestrascol, estado, variedades_id) VALUES (?,?,?,?,?,?,?)`
 
-        // let sql= 'INSERT INTO usuarios( nombre, apellido, numero_documentos,telefono, correo_electronico, user_password, tipo_documento, rol, cargo) VALUES (?,?,?,?,?,?,?,?,?)';
-
-        const [rows] = await pool.query(sql, [ data.fecha, data.nombre, data.cantidad,data.lotes_id ]); 
+        const [rows] = await pool.query(sql, [ data.fecha, data.nombre, data.cantidad,data.lotes_id,data.muestrascol,data.estado,data.variedades_id ]); 
         
         if (rows.affectedRows > 0) {
             res.status(200).json({
@@ -20,8 +18,8 @@ export const guardarMuestra = async (req, res) => {
             );
 
         } else {
-            res.status(400).json({
-                "status": 400,
+            res.status(401).json({
+                "status": 401,
                 "message": "La muestra no se registro"
             }
             );
@@ -44,10 +42,10 @@ export const actualizarMuestra = async (req, res) => {
         // let { fecha_creacion, cantidad,lotes_id} = req.body;
         let data = req.body;
 
-        let sql = `UPDATE muestras SET fecha_creacion='${data.fecha_creacion}',cantidad='${data.cantidad}',lotes_id='${data.lotes_id}' where id='${id}'`
+        let sql = `UPDATE muestras SET fecha_creacion='${data.fecha_creacion}',nombre='${data.nombre}',cantidad='${data.cantidad}',lotes_id='${data.lotes_id}',muestrascol='${data.muestrascol}',estado='${data.estado}',variedades_id='${data.variedades_id}' where id='${id}'`
 
 
-        // let sql = `UPDATE muestras SET fecha_creacion='${fecha_creacion}',cantidad='${cantidad}',lotes_id='${lotes_id}' where id='${id}'`
+ 
 
 
         console.log("user",sql);
@@ -95,7 +93,7 @@ export const buscarMuestra = async (req, res) => {
 export const listarMuestras = async (req, res) => {
 
     try {
-        const [result] = await pool.query("SELECT m.id, m.fecha_creacion, m.cantidad, l.nombre as lote,f.nombre as finca, concat(u.nombre,' ',u.apellido)  as propietario from muestras m JOIN lotes l ON m.lotes_id = l.id JOIN fincas f ON l.fincas_id = f.id JOIN usuarios u ON f.usuarios_id = u.id");
+        const [result] = await pool.query("SELECT m.id, m.fecha_creacion, m.cantidad, m.nombre as nombre, l.nombre as lote,f.nombre as finca, concat(u.nombre,' ',u.apellido)  as propietario from muestras m JOIN lotes l ON m.lotes_id = l.id JOIN fincas f ON l.fincas_id = f.id JOIN usuarios u ON f.usuarios_id = u.id");
         res.status(200).json(result);
     } catch (err) {
         res.status(500).json({
--- back-end/src/routers/muestra.router.js
+++ back-end/src/routers/muestra.router.js
@@ -4,7 +4,7 @@ import { guardarMuestra,actualizarMuestra,buscarMuestra,listarMuestras } from ".
 
 const muestraRoute = Router()
 
-muestraRoute.post("/registrarMuestra",guardarMuestra);
+muestraRoute.post("/registrar",guardarMuestra);
 muestraRoute.put("/actualizarMuestra/:id",actualizarMuestra);
 muestraRoute.get("/buscarMuestra/:id",buscarMuestra);
 muestraRoute.get("/listarMuestra",listarMuestras);
