ffd9f2242b73e01a86487f2962fa479c9beded4e
danielcordoba12 <dcordobaruiz@gmail.com>
2023-11-16 22:00:52 -0500

    validar token dcr

--- back-end/src/routers/resultado.router.js
+++ back-end/src/routers/resultado.router.js
@@ -2,14 +2,15 @@ import { Router } from "express";
 
 import { guardarResultado, buscarResultado,listarResultados,eliminarResultado,actualizarResultado } from "../controllers/resultado.controller.js";
 import { validarResultado } from "../validation/resultado.validator.js";
+import { validartoken } from "../controllers/autenticacion.controller.js";
 
 const resultadoRoute = Router();
 
 
-resultadoRoute.post("/registrar",validarResultado,guardarResultado);
+resultadoRoute.post("/registrar",validartoken,validarResultado,guardarResultado);
 resultadoRoute.get("/listar",listarResultados);
 resultadoRoute.get("/buscar/:id",buscarResultado);
-resultadoRoute.put("/update/:id",validarResultado,actualizarResultado);
+resultadoRoute.put("/update/:id",validartoken,validarResultado,actualizarResultado);
 resultadoRoute.delete("/delete/:id",eliminarResultado);
 
 
