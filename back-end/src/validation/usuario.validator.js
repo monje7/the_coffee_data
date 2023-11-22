250e8218e5b27de44ea3658ff52319a7292c8097
Luis1080931 <luisu0616@gmail.com>
2023-11-22 07:28:07 -0500

    validacion usuarios

--- back-end/src/validation/usuario.validator.js
+++ back-end/src/validation/usuario.validator.js
@@ -1,13 +1,41 @@
 import { check } from "express-validator";
 
-export const validatorUsuario =[
-    check('nombre','Ingrese un nombre con minimo 4 caracteres y maximo 10').isLength({min:4,max:20}).notEmpty(),
-    check('apellido','Ingrese un nombre con minimo 4 caracteres y maximo 10').isLength({min:4,max:30}).notEmpty(),
-    check('documento','Ingrese un dcouemnto con maximo 10 digitos').isNumeric().isLength({min:4,max:10}).notEmpty(),
-    check('telefono','Ingrese un telefono con minimo 10 digitos y maximo 15').isNumeric().isLength({min:10,max:15}).notEmpty(),
-    check('correo', 'Ingrese un correo electrónico válido').isEmail().notEmpty(),
-    check('contraseña','Ingrese una contraseña con minimo 5 caracteres y maximo 15').isNumeric().isLength({min:5,max:15}).notEmpty(),
-    check('tipo_identificacion','Ingrese un tipo de identificacion válido, solamente tarjeta de identidad o cedula de ciudadania').matches(/^(tarjeta de identidad|cedula de ciudadania)$/).notEmpty(),
-    check('rol','Ingrese un tipo de rol válido, solamente rol de catador,cafetero,administrador').matches(/^(catador|cafetero|administrador)$/).notEmpty(),
-    check('cargo','Ingrese un cargo válido, solamente cargo de instructor,aprendiz,cliente,administrador').matches(/^(instructor|aprendiz|cliente|administrador)$/).notEmpty(),
-]   
+export const validatorUsuario = [
+  check('nombre')
+    .notEmpty().withMessage('Ingrese un nombre con mínimo 4 caracteres y máximo 10')
+    .isLength({ min: 4, max: 20 }),
+
+  check('apellido')
+    .notEmpty().withMessage('Ingrese un apellido con mínimo 4 caracteres y máximo 10')
+    .isLength({ min: 4, max: 30 }),
+
+  check('documento')
+    .notEmpty().withMessage('Ingrese un documento')
+    .isNumeric().withMessage('El documento debe ser numérico')
+    .isLength({ max: 10 }).withMessage('Ingrese un documento con máximo 10 dígitos'),
+
+  check('telefono')
+    .notEmpty().withMessage('Ingrese un teléfono')
+    .isNumeric().withMessage('El teléfono debe ser numérico')
+    .isLength({ min: 10, max: 15 }).withMessage('Ingrese un teléfono con mínimo 10 dígitos y máximo 15'),
+
+  check('correo')
+    .notEmpty().withMessage('Ingrese un correo electrónico')
+    .isEmail().withMessage('Ingrese un correo electrónico válido'),
+
+  check('contraseña')
+    .notEmpty().withMessage('Ingrese una contraseña')
+    .isLength({ min: 5, max: 15 }).withMessage('Ingrese una contraseña con mínimo 5 caracteres y máximo 15'),
+
+  check('tipo_identificacion')
+    .notEmpty().withMessage('Ingrese un tipo de identificación')
+    .matches(/^(tarjeta de identidad|cedula de ciudadania)$/).withMessage('Ingrese un tipo de identificación válido: tarjeta de identidad o cedula de ciudadania'),
+
+  check('rol')
+    .notEmpty().withMessage('Ingrese un tipo de rol')
+    .matches(/^(catador|cafetero|administrador)$/).withMessage('Ingrese un tipo de rol válido: catador, cafetero o administrador'),
+
+  check('cargo')
+    .notEmpty().withMessage('Ingrese un cargo')
+    .matches(/^(instructor|aprendiz|cliente|administrador)$/).withMessage('Ingrese un cargo válido: instructor, aprendiz, cliente o administrador')
+];
