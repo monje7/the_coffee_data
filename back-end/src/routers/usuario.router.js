6072bf23eac2d4661cb4a8b2fe7d04f730ee377b
Luis1080931 <luisu0616@gmail.com>
2023-11-23 09:22:28 -0500

    confirmacion de usuario controller juan

--- back-end/src/routers/usuario.router.js
+++ back-end/src/routers/usuario.router.js
@@ -1,16 +1,20 @@
 
 import {Router} from "express";
 
-import {guardarUsuario,listarUsuario,buscarUsuarios,actualizarUsuario} from "../controllers/usuarios.controller.js";
+import {guardarUsuario,listarUsuario,ActivarUsuario,desactivarUsuario,buscarUsuarios,actualizarUsuario} from "../controllers/usuarios.controller.js";
 import { validatorUsuario } from "../validation/usuario.validator.js";
+import { validartoken } from "../controllers/autenticacion.controller.js";
 
 
 const usuarioRoute = Router();
 
-usuarioRoute.post("/registrar",validatorUsuario,guardarUsuario);
+usuarioRoute.post("/registrar",validartoken,validatorUsuario,guardarUsuario);
 usuarioRoute.get("/listarusuario",listarUsuario);
 usuarioRoute.get("/buscarusuario/:id",buscarUsuarios);
-usuarioRoute.put("/actualizar/:id",validatorUsuario,actualizarUsuario);
+usuarioRoute.patch("/activar/:id",validartoken,ActivarUsuario);
+usuarioRoute.patch("/desactivar/:id",validartoken,desactivarUsuario);
+usuarioRoute.put("/actualizar/:id",validartoken,validatorUsuario,actualizarUsuario);
+
 
 
 
