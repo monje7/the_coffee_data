825bfc7cda934d1ecce65a7ae57eff670c08fb98
danielcordoba12 <dcordobaruiz@gmail.com>
2023-11-29 17:42:35 -0500

    i bootstrap

---
+++ frontend-react/src/components/Resultados.jsx
@@ -0,0 +1,14 @@
+import React,{useEffect,useRef} from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
+
+
+const Muestra = () => {
+    return(
+
+    <Button className='btn btn-primary'>DJFNGD</Button>
+
+    )
+}
+
+export default Muestra
