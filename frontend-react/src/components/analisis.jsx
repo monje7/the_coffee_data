30724bee459e023cb98988418d01ca474374a14b
Danna947 <dannalopez072004@gmail.com>
2023-11-30 11:40:15 -0500

    actualizciones

---
+++ frontend-react/src/components/analisis.jsx
@@ -0,0 +1,20 @@
+import React,{useEffect,useRef} from "react";
+import { useNavigate } from "react-router-dom";
+import { Table} from "react-bootstrap";
+import Image from 'react-bootstrap/Image';
+
+
+
+function FluidExample() {
+  return <Image src="holder.js/100px250" fluid />;
+
+
+
+
+  
+}
+
+export default FluidExample;
+
+
+
