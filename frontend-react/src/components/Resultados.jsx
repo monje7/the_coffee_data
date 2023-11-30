95f0a92fca751f67b9cec0d89f839f8d53450367
danielcordoba12 <dcordobaruizv@gmail.com>
2023-11-30 11:37:59 -0500

    listar muestra

--- frontend-react/src/components/Resultados.jsx
+++ frontend-react/src/components/Resultados.jsx
@@ -1,26 +1,50 @@
-import React,{useEffect,useRef} from "react";
+import React,{useEffect,useState} from "react";
 import api from "../services/api";
 import { useNavigate } from "react-router-dom";
-import { Table } from "react-bootstrap";
-
+import { Table} from "react-bootstrap";
 
 function Muestra() {
-    return (
+    const [tasks,setTasks] = useState([]);
 
-  useEffect(() => {
-    const fetchTasks = async () => {
-        try {
-            const response = await api.get('resultado/listar');
-            setTasks(response.data);
-            console.log(response.data)
-        }catch (error) {
-            console.error('Error fetching tasks',error) 
-        }
-    };
+    useEffect(() => {
+        const fetchTasks = async () => {
+            try {
+                const response = await api.get('resultado/listar');
+                setTasks(response.data);
+                console.log(response.data)
+            }catch (error) {
+                console.error('Error fetching tasks',error) 
+            }
+        };
+        fetchTasks();
+    },[]);
+    
 
+    return(
+        <Table striped bordered hover>
+        <thead>
+        <tr>
+            <th>Analisis</th>
+            <th>fecha</th>
+            <th>Valor</th>
+            <th>Variable</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            {/* Aqui va el TASK */}
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+
+        
+        </tbody>
+    </Table>
     );
 
-  
+
 }
 
 export default Muestra;
