67896ad4b673e1212633d670ab4dc2ffcb7a2bc5
Danna947 <dannalopez072004@gmail.com>
2023-12-01 09:02:57 -0500

    actaulizaciones analisis

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Muestra from './components/Resultados'
 import Dashboard from './components/dashboard'
+import Analisiss from './components/Analisiss'
 
 function App(){
   return(
@@ -11,6 +12,7 @@ function App(){
       <Routes>
       <Route path='/' element={<Dashboard/>}/>
         <Route path='/Muestra' element={<Muestra/>}/>
+        <Route path='/Analisiss' element={<Analisiss/>}/>
       </Routes>
     </>
   )
---
+++ frontend-react/src/components/Analisiss.jsx
@@ -0,0 +1,21 @@
+import React,{useEffect,useRef} from "react";
+import api from "../services/api";
+import { useNavigate } from "react-router-dom";
+import Nav from 'react-bootstrap/Nav';
+import '../style/analisis.css';
+import {faUser,faHelmetSafety,faClockRotateLeft,faMugSaucer,faToolbox,faMagnifyingGlassChart,faChartColumn,faPhone,faSliders}  from'@fortawesome/free-solid-svg-icons'
+
+function Analisiss() {
+  return (
+   
+        <div className="main-cont">
+           <nav className="nav">
+              <ul>
+                <li><a href=""></a>scdnsefc</li>
+              </ul>
+           </nav>
+        </div>
+  );
+}
+
+export default Analisiss;
